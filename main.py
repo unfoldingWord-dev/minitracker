643390ffc151589c12d62e91aededa4ded0d84b2
Jaap-Jan Swijnenburg <jakobaleksandrovich@gmail.com>
2022-10-14 15:57:53 +0200

    Implemented generic method for tracking, using config file 'trackers.conf'

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import flask_cors
 from flask import Flask, request, render_template, Response, send_from_directory
 from jinja2 import Template
 import waitress
@@ -7,6 +8,8 @@ import graphyte
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import json
+from flask_cors import CORS
 
 load_dotenv()
 
@@ -20,9 +23,15 @@ class MiniTracker:
         if not graphite_prefix:
             raise RuntimeError('Missing environment variable GRAPHITE_PREFIX')
 
+        # Init graphite
         graphyte.init(graphite_host, prefix=graphite_prefix)
 
+        # Init logger
         self.logger = self.__init_logger()
+
+        # Loading configuration file
+        with open('conf/trackers.conf') as f_trackers:
+            self.tracker_config = json.load(f_trackers)
 
     def __init_logger(self):
         if os.getenv('STAGE', False) == 'dev':
@@ -38,6 +47,14 @@ class MiniTracker:
 
         this_logger = logging.getLogger()
         return this_logger
+
+    def get_allowed_origins(self):
+        # Get allowed CORS origins.
+        # If we have no allowed origins defined, we allow everything.
+        if 'allowed_origins' in self.tracker_config:
+            return self.tracker_config['allowed_origins']
+
+        return '*'
 
     def __send_metric(self, metric, value):
         self.logger.debug('Metric: ' + metric + ', ' + str(value))
@@ -98,6 +115,116 @@ class MiniTracker:
 
         return False
 
+    def __load_tracker_config(self, mt_id):
+        for config in self.tracker_config['trackers']:
+            if config['mt_id'] == mt_id:
+                return config
+
+        return None
+
+    def __bot_detected(self, useragent):
+        # TODO. Make a better bot detection mechanism, probably based on a bot list
+        if re.search("[bB]ot", useragent):
+            return True
+
+        return False
+
+    def __convert_parameter_to_metric(self, url_param, parsers):
+        for parser in parsers:
+            if re.fullmatch(parser['search'], url_param):
+                metric = re.sub(parser['search'], parser['metric'], url_param)
+                self.logger.debug("URL param '{}' converted to metric '{}'".format(url_param, metric))
+                return metric
+
+        return None
+
+    def __sanitize_url_param(self, url_param):
+        # Sanitize for TSDB purposes
+        new_param = url_param.replace('.', '_')
+        return new_param
+
+    def track(self):
+        self.logger.debug(request.full_path)
+        base_warning = "400 - Bad request"
+        mandatory_param_missing = False
+
+        mt_id = request.args.get('mt_id')
+        if mt_id:
+            config = self.__load_tracker_config(mt_id)
+            if config:
+                graphite_prefix = config['metric_prefix']
+
+                referrer = request.headers.get('Referer')
+                useragent = request.headers.get('User-Agent')
+
+                # Either bots are allowed, or no bot is detected
+                if config['allow_bots'] or not self.__bot_detected(useragent):
+
+                    if referrer and config['referer'] in referrer:
+
+                        # At least one url parameter is required
+                        if config['url_parameters'] and len(config['url_parameters']) > 0:
+                            self.logger.debug(config)
+
+                            lst_metrics = list()
+
+                            for url_param in config['url_parameters']:
+
+                                value = request.args.get(url_param)
+                                if value:
+                                    param_config = config['url_parameters'][url_param]
+
+                                    if 'converters' in param_config:
+                                        conversions = param_config['converters']
+                                        metric = self.__convert_parameter_to_metric(value, conversions)
+                                    else:
+                                        metric = self.__sanitize_url_param(value)
+
+                                    # Insert metric at correct position
+                                    pos = 0 # Default if not given
+                                    if 'metric_position' in param_config:
+                                        pos = param_config['metric_position']
+                                    lst_metrics.insert(pos, metric)
+
+                                else:
+                                    if 'mandatory' in config['url_parameters'][url_param]:
+                                        if config['url_parameters'][url_param]['mandatory'] is True:
+                                            mandatory_param_missing = True
+                                            self.logger.warning('Mandatory URL param \'{}\' not found in URI string'.format(url_param))
+
+                            # Send off the metric
+                            if not mandatory_param_missing:
+                                metric = '.'.join(lst_metrics)
+
+                                # Send metric and response
+                                self.__send_metric(graphite_prefix + '.' + metric, 1)
+
+                                response = Response('', status=204)
+                                return response
+
+                        else:
+                            self.logger.warning("{}: No url_parameters defined".format(base_warning))
+
+                    else:
+                        self.logger.warning("{}: Invalid referer '{}'".format(base_warning, referrer))
+
+                else:
+                    self.logger.warning("{}: Bot detected '{}'".format(base_warning, useragent))
+
+            else:
+                self.logger.warning("{}: mt_id '{}' cannot be found in configuration".format(base_warning, mt_id))
+
+        else:
+            # No mt_id found.
+            self.logger.warning("{}: No mt_id found in URL".format(base_warning))
+
+        # Catch all
+        status_code = 400
+        text = self.__generate_error_page(status_code, 'Bad request')
+
+        response = Response(text, status=status_code)
+        return response
+
     def download_logger(self):
         my_prefix = 'obs.website.downloads'
 
@@ -156,6 +283,8 @@ class MiniTracker:
 
 # Initialize
 app = Flask(__name__, static_folder='static')
+cors = CORS(app)
+
 mt = MiniTracker()
 
 
@@ -170,9 +299,15 @@ def track_home():
     return mt.home()
 
 
+@app.route('/track')
+@flask_cors.cross_origin(origins=mt.get_allowed_origins())
+def track():
+    return mt.track()
+
+
 @app.route("/favicon.ico")  # Load the favicon
 def fav():
-    return send_from_directory(app.static_folder, 'favicon.ico')  # for sure return the file
+    return send_from_directory(app.static_folder, 'favicon.ico')
 
 
 # Main
